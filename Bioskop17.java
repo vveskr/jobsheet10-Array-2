f334d2a37f868d4266ea175dc6847d8b6414ddcc
vveskr <vveskr@gmail.com>
2023-11-03 10:08:06 +0700

    update

--- Bioskop17.java
+++ Bioskop17.java
@@ -17,8 +17,8 @@ public class Bioskop17{
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1] );
 
         System.out.println(penonton.length);
-        for(int i = 0; i<penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for(String[] barisPenonton : penonton){
+            System.out.println("Panjang baris:" + barisPenonton.length);
         }
     }
 }
